99210562b947e10ff739de355738fea2970f1efb
ebhevama <ebhevama@gmail.com>
2019-12-06 17:11:38 -0500

    using js to define and print variables

---
+++ 5-javascript/my_first_javascript_variable.js
@@ -0,0 +1,19 @@
+//answer file my_first_javascript_variable.js
+//Typing in my browser console
+//let x=2
+// >> undefined                             //console's output
+// console.log(x)
+// >> 2                                     //console's output
+// >> undefined                             //console's output
+//let variable_test = "Holberton"
+// >> undefined                             //console's output
+//console.log(variable_test)
+// >> undefined                             //console's output
+// >> Holberton                             //console's output
+
+//Task2's solution
+let my_variable = "I love Javascript!"      //define my_variable
+// >> undefined                             //console's output
+console.log(my_variable)                    //print my_variable
+// >> undefined                             //console's output
+// >> I love Javascript!                    //console's output
