ce240099cefe0173d495c3adfccee967a9d4e902
ebhevama <ebhevama@gmail.com>
2019-12-07 01:45:19 -0500

    creating arrays in js

---
+++ 5-javascript/102-array.js
@@ -0,0 +1,18 @@
+//Task6's solution: answer file 102-array.js
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Creating arrays in js</title>
+  </head>
+  <body>
+    <script>
+    // Creating variables
+    var dwarvesn = ["Doc", "Grumpy", "Happy", "Sleepy", "Bashful", "Sneezy",
+    "Dopey"];
+
+    // Printing variable values
+    document.write(dwarvesn);
+    </script>
+  </body>
+</html>
