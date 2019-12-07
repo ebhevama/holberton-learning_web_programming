c164ccce61fa95bcbbf61d366e6a38d4338ab84a
ebhevama <ebhevama@gmail.com>
2019-12-07 01:56:58 -0500

    advanced arrays in js

---
+++ 5-javascript/103-more_array.js
@@ -0,0 +1,16 @@
+//Task7's solution: answer file 102-array.js
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>advanced arrays in js</title>
+  </head>
+  <body>
+    <script>
+      var dwarvesn = ["Doc", "Grumpy", "Happy", "Sleepy", "Bashful", "Sneezy",
+      "Dopey"];                       //Creating variables
+
+      document.write(dwarvesn);       //Printing variable values
+    </script>
+  </body>
+</html>
