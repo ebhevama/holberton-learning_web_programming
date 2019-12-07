98e8635eb6a62059fa9dbbd64020ed202799c80f
ebhevama <ebhevama@gmail.com>
2019-12-06 22:36:40 -0500

    console's commands to implement a for loop in JavaScript

---
+++ 5-javascript/100-my_first_for_loop.js
@@ -0,0 +1,16 @@
+//Task4's solution: answer file 100-my_first_for_loop.js
+//implementing a while loop in JavaScript
+// <!DOCTYPE html>
+// <html lang="en">
+//   <head>
+//     <meta charset="utf-8">
+//     <title>While loop in JavaScript</title>
+//   </head>
+//   <body>
+//     <script>
+      for(var i = 100; i >= 1; i--) { 
+      console.log(i)                        //Print command in console***+++
+      }
+//     </script>
+//   </body>
+// </html>
