a3def068f20aa3ce6011f6591900554303a6d04f
ebhevama <ebhevama@gmail.com>
2019-12-07 03:31:53 -0500

    advanced arrays in js

--- 5-javascript/103-more_array.js
+++ 5-javascript/103-more_array.js
@@ -7,10 +7,10 @@
   </head>
   <body>
     <script>
-      var dwarvesn = ["Doc", "Grumpy", "Happy", "Sleepy", "Bashful", "Sneezy",
-      "Dopey"];                       //Creating variables
-
-      document.write(dwarvesn);       //Printing variable values
+      for(var i = 1; i <= 100; i++) {
+        if ((i % 2) == 0)               //%: remainder, ==: equal to
+        document.writeln(i);  //adds a new line after each statement
+      }                       //still missing include an array &/or function
     </script>
   </body>
 </html>
