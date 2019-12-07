7c1f37ae8e2610566748addb93791b57523fd870
ebhevama <ebhevama@gmail.com>
2019-12-06 21:12:30 -0500

    implementing a while loop in JavaScript

---
+++ 5-javascript/3-my_first_loop.js
@@ -0,0 +1,18 @@
+//Task3's solution: answer file 3-my_first_loop.js
+//implementing a while loop in JavaScript
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>While loop in JavaScript</title>
+</head>
+<body>
+    <script>
+    var i = 0;
+    while(i <= 10) {
+        document.write("<p>" + i + "</p>");
+        i++;
+    }
+    </script>
+</body>
+</html>*
