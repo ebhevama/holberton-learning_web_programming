1735dbceceb18c871ec2aa01176e725c089e7aea
ebhevama <ebhevama@gmail.com>
2019-12-10 00:03:12 -0500

    js if - else statement

---
+++ 6-javascript_continued/2-what_else.js
@@ -0,0 +1,14 @@
+// //Task1's solution: answer file 1-condition.js
+//Using "node" in the command line console: node file.js parameter
+//Then "enter" to execute the order
+
+      let x = process.argv[2];        //Creating variables
+
+      if(x>5) {
+        console.log("The value is greater than 5");
+      } else {
+        console.log("The value is equal to 5 or lower");
+      }
+
+// >> The value is greater than 5           //console's output if x>5***
+// >> The value is equal to 5 or lower      //console's output if x<=5***
