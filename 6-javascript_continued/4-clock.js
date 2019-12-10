89685ef802745b95c2af06571ab36aa485ef86e4
ebhevama <ebhevama@gmail.com>
2019-12-10 01:49:52 -0500

    nested loops-virtual clock

---
+++ 6-javascript_continued/4-clock.js
@@ -0,0 +1,14 @@
+// //Task4's solution: answer file 3-grading_system.js
+//Using "node" in the command line console: node file.js
+//Then "enter" to execute the order
+
+  var h = 0;                          //defining variable: h (hour), m (minute)
+
+  while(h<24) {
+    console.log(h)                    //Print command in console
+    for (var m = 0; m<=59; m++) {
+      // console.log(h,m)             //Print command in console
+      console.log(m)                  //Print command in console
+    }
+    h++;
+  }
