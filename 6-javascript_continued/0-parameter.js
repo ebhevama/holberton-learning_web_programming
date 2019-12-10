d3ccd565e2d4cad15c97d04e7853c1279700eaad
ebhevama <ebhevama@gmail.com>
2019-12-09 23:02:27 -0500

    passed arguments

---
+++ 6-javascript_continued/0-parameter.js
@@ -0,0 +1,16 @@
+//Task0's solution: answer file 102-array.js
+// <!DOCTYPE html>
+// <html lang="en">
+//   <head>
+//     <meta charset="utf-8">
+//     <title>Passing an argument to my js script</title>
+//   </head>
+//   <body>
+    //Store a value passed by the user to the script in the variable my_parameter.
+    //Note it takes the 2nd element corresponding to the 1st parameter. Third
+    //element would correspond to second parameter
+    let my_parameter = process.argv[2];
+
+    console.log("Here is the first passed parameter: " + my_parameter) // Print the my_parameter variable
+//   </body>
+// </html>
