7d5d0d20e4bea8f97f9f2dd0cf4f6ae74f56ad20
ebhevama <ebhevama@gmail.com>
2019-12-10 00:02:16 -0500

    js if statement

--- 6-javascript_continued/1-condition.js
+++ 6-javascript_continued/1-condition.js
@@ -1,17 +1,12 @@
 // //Task1's solution: answer file 1-condition.js
-// <!DOCTYPE html>
-// <html lang="en">
-//   <head>
-//     <meta charset="utf-8">
-//     <title>js If statement</title>
-//   </head>
-//   <body>
-//     <script>
-      let x = process.argv[2];    //Creating variables
+//Using "node" in the command line console: node file.js parameter
+//Then "enter" to execute the order
+
+      let x = process.argv[2];        //Creating variables
 
       if(x>5) {
         console.log("The value is greater than 5");
       }
-//     </script>
-//   </body>
-// </html>
+
+// >> The value is greater than 5        //console's output if x>5***
+// >>                                    //console's output if x<=5***
