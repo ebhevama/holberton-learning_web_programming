31ca951de8abce552e3731604363a7e14fd3eb40
ebhevama <ebhevama@gmail.com>
2019-12-10 00:02:33 -0500

    passed arguments

--- 6-javascript_continued/0-parameter.js
+++ 6-javascript_continued/0-parameter.js
@@ -1,16 +1,12 @@
-//Task0's solution: answer file 102-array.js
-// <!DOCTYPE html>
-// <html lang="en">
-//   <head>
-//     <meta charset="utf-8">
-//     <title>Passing an argument to my js script</title>
-//   </head>
-//   <body>
-    //Store a value passed by the user to the script in the variable my_parameter.
-    //Note it takes the 2nd element corresponding to the 1st parameter. Third
-    //element would correspond to second parameter
-    let my_parameter = process.argv[2];
+//Task0's solution: answer file 0-parameter.js
+//Using "node" in the command line console: node file.js parameter
+//Then "enter" to execute the order
 
-    console.log("Here is the first passed parameter: " + my_parameter) // Print the my_parameter variable
-//   </body>
-// </html>
+//Store a value passed by the user to the script in the variable my_parameter.
+//Note it takes the 2nd element corresponding to the 1st parameter. Third
+//element would correspond to second parameter
+  let my_parameter = process.argv[2];
+
+  console.log("Here is the first passed parameter: " + my_parameter) // Print "my_parameter" variable
+
+// >> Here is the first passed parameter: [my_parameter]        //console's output
